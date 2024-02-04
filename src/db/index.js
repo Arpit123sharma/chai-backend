322e97287808f8c6f86f141840b928d6b948b8bc
Arpit123sharma <138675911+Arpit123sharma@users.noreply.github.com>
2024-02-04 14:07:45 +0530

    remove vjwt

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+
 
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`mongodb://127.0.0.1:27017/videotube`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        console.log(`\n MongoDB connected !! `);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
         process.exit(1)
