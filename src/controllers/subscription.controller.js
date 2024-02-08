98086502b9f2bf5dcbaf446882e09ead28bd0585
Arpit123sharma <138675911+Arpit123sharma@users.noreply.github.com>
2024-02-08 18:30:00 +0530

    write subscribtion controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -44,11 +44,41 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    if (!channelId.trim()) {
+        throw new ApiError(400,"channel id is required")
+    }
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400,"channel id is invalid")
+    }
+    const subscribers = await Subscription.find({
+        channel:channelId
+    })
+    if (!subscribers) {
+        throw new ApiError(400,"this channel has no subscribers")
+    }
+    return res
+    .status(200)
+    .json( new ApiResponse(200,subscribers,"subscribers list fetched successfully!!"))
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    if (!subscriberId.trim()) {
+        throw new ApiError(400,"subscriber id is required")
+    }
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400,"subscriber id is invalid")
+    }
+    const channels = await Subscription.find({
+        subscriber:subscriberId
+    })
+    if (!channels) {
+        throw new ApiError(400,"user does not subscribed to any channel")
+    }
+    return res
+    .status(200)
+    .json( new ApiResponse(200,channels,"channel list fetched successfully!!"))
 })
 
 export {
